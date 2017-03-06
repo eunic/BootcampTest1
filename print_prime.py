6ff83afd4ced7fdc48b8e29ac008f927484a371f
Eunice <victorywanjiru@gmail.com>
2017-03-06 20:16:47 +0300

    This is are commits for prime number

--- print_prime.py
+++ print_prime.py
@@ -1,20 +1,20 @@
-j = 0
+j =0 
+isprime = (False)
+n = 10
+def primenum(i,j,op):
 
-isprime = False
+	for i in range(0,n):
+		
+			for j in range(2,n-i):
 
-def primenum(n):
+				if i <=1:
+					pass
 
-	for i in range(0,n+1):
+				elif i%j != 0 and i%i == 0 and i%1 ==0:
 
-		for j in range(2,n):
-		 	
-			if i%j != 0 and i % 1 == 0 and i % i == 0:
+						print(i)
 
-				
-				
-		print(i)
+						break
 
-	if(isprime):
-
-		print(i)
-primenum(9)
+	
+primenum(3,3,"%")
--- test_solution.py
+++ test_solution.py
@@ -4,6 +4,11 @@ import print_prime
 class TestSoution(unittest.TestCase):
 
 	def primenum(self):
+		self.assertTrue(printnum(3,3,"%"),0)
+		self.assertTrue(printnum(3,1,"%"),0)
+		self.assertTrue(printnum(5,2,"%"),2.5)
+		self.assertTrue(i != 0)
 
-if __name__ == "__main__":
-	unittest.main()
+
+	if __name__ == "__main__":
+		unittest.main()
